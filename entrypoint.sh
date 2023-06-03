889ba3142637650ab854c5a2ac4fb62ada001827
Blake Oliver <oliver22213@me.com>
2023-06-02 20:55:55 -0600

    Update entrypoint.sh to use a variable and add the US as an example line to only download specific POIs

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,15 @@ if [ ! -d "/photon/photon_data/elasticsearch" ]; then
     echo "Downloading search index"
 
     # Let graphhopper know where the traffic is coming from
-    USER_AGENT="docker: tonsnoei/photon-geocoder"
-    # If you want to install a specific region only, enable the line below and disable the current 'wget' row.
+    USER_AGENT="docker: openscape-community/photon-geocoder"
+    # If you want to install a specific region only, enable the line below and disable the current 'URL=' line.
     # Take a look at http://download1.graphhopper.com/public/extracts/by-country-code for your country
-    # wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/extracts/by-country-code/nl/photon-db-nl-latest.tar.bz2 | bzip2 -cd | tar x
-    wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | bzip2 -cd | tar x
+
+# POIs for the world!
+URL="https://download1.graphhopper.com/public/photon-db-latest.tar.bz2"
+# Download POI data only for a country (by code); E.G. the US
+#URL="https://download1.graphhopper.com/public/extracts/by-country-code/us/photon-db-us-latest.tar.bz2"
+    wget --user-agent="$USER_AGENT" -O - $URL | bzip2 -cd | tar x
 fi
 
 # Start photon if elastic index exists
